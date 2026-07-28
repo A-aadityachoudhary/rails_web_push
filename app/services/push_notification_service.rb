37220ec18fa183760103a141fedb52d1424a036a
A-aadityachoudhary <aadityach812@gmail.com>
2026-07-28 15:22:47 +0530

    added mannuly keys

--- app/services/push_notification_service.rb
+++ app/services/push_notification_service.rb
@@ -22,8 +22,8 @@ class PushNotificationService
 
       vapid: {
         subject: "mailto:admin@example.com",
-        public_key: Rails.application.credentials.web_push[:publicKey],
-        private_key: Rails.application.credentials.web_push[:privateKey]
+        public_key: "BOvHJNqfb9MgmzR96e49QKLP9tzIELYaSQPuj5n9K-kh24byeHYtwrE-7V7wdRN3a2PlxWj6xkV1sAE0jahJDm0="
+        private_key: "8xcrfmRSqTW9kS5GnAOa5LsHl506cwGZ9o5au5guhtk="
       }
     )
   rescue WebPush::ExpiredSubscription
