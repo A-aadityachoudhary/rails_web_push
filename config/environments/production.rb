ecce30dc9f72e7321f303d7b7cd464a4ba7d49be
A-aadityachoudhary <aadityach812@gmail.com>
2026-08-06 16:31:33 +0530

    using async for production jobs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.active_job.queue_adapter = :solid_queue
+  config.active_job.queue_adapter = :async
 
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
